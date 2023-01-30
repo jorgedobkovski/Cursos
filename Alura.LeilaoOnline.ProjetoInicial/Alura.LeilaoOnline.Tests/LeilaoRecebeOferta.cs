128440984e1b90b38919be7404abded9cf70d6aa
Jorge <dobkovskijorge@gmail.com>
2023-01-30 13:12:56 -0400

    teorizando com xunit

---
+++ Alura.LeilaoOnline.ProjetoInicial/Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -0,0 +1,33 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoRecebeOferta
+    {
+        [Fact]
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulan0", leilao);
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(fulano, 800);
+            leilao.TerminaPregao();
+
+            //Act - método sob teste
+            leilao.RecebeLance(fulano, 1000);
+
+            //Assert
+            var valorEsperado = 2;
+            var valorObtido = leilao.Lances.Count();
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
---
+++ Alura.LeilaoOnline.ProjetoInicial/Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -0,0 +1,56 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTerminaPregao
+    {
+        [Theory]
+        [InlineData(1200, new double[] {800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] {800, 900, 1000, 990 })]
+        [InlineData(800, new double[] {800})]
+
+        public static void RetornaMaiorValorDadoLeilaoComPeloMenosUmLance(double valorEsperado, double[] valores)
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            foreach(var valor in valores)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+
+        [Fact]
+        public void RetornaZeroDadoLeilaoSemLances()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+
+            //act - método sob teste
+            leilao.TerminaPregao();
+
+            //assert
+            var valorEsperado = 0;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
